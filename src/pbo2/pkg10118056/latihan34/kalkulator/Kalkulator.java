cdd5ab7a25e4e5adaf510db784edb2398adcd814
Mirraz Ibrahim <55575435+Mirrazibrahim@users.noreply.github.com>
2019-10-17 10:48:26 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118056/latihan34/kalkulator/Kalkulator.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118056.latihan34.kalkulator;
+
+/**
+ *
+ * @author 
+ * NAMA     : Mirrazibrahim
+ * KELAS    : IF-2
+ * NIM      : 10118056
+ */
+public class Kalkulator {
+    public double value1,value2;
+    
+    public double tambahBilangan(){
+        return value1+value2;
+    }
+    public double kurangBilangan(){
+        return value1-value2;
+    }
+    public double kaliBilangan(){
+        return value1*value2;
+    }
+    public double bagiBilangan(){
+        return value1/value2;
+    }
+    public double sisaBilagan(){
+        return value1%value2;
+    }
+    
+}
